39593be4ee9264736ba4aa56d50f610ed1a14bb5
mo <286711902@qq.com>
2021-05-31 11:00:10 +0800

    back up

--- sftp_bwg/sftp_bwg.sh
+++ sftp_bwg/sftp_bwg.sh
@@ -1,3 +1,9 @@
-sftp -P 26236 root@104.225.153.196
+##密码
 echo "hwESMcR9Y6hM"
+##文件下载
 echo "get Linux_filepath  windows_path"
+##文件上传
+echo "put Linux_filepath  windows_path"
+##其他
+echo "ls、lls;cd、lcd;pwd、lpwd"
+sftp -P 26236 root@104.225.153.196
